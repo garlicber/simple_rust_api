c3dccd372cbc2002b7700160c7bce0851b7af99e
Jakob Krause <ulukaiz@hotmail.com>
2015-08-15 20:41:33 +0200

    start implement message parsing

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,25 @@ use iron::status;
 use router::{Router};
 
 fn main() {
+    let mut messages = Vec::new();
     let mut router = Router::new();
+
+    let all_messages_handler = |req: &mut Request| -> IronResult<Response> {
+            Ok(Response::with((status::Ok, messages.connect("\n"))))
+    };
+
+    let handler =|req: &mut Request| -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+        messages.push(*query);
+            Ok(Response::with((status::Ok, "Hallo ".to_string() + *query)))
+    };
+
     router.get("/", handler);
-    router.get("/:query", handler);
+    router.get("/msg/:query", handler);
+    router.get("/all", all_messages_handler);
 
     Iron::new(router).http("localhost:3000").unwrap();
 
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>()
-            .unwrap().find("query").unwrap_or("/");
-        Ok(Response::with((status::Ok, *query)))
-    }
+
 }
