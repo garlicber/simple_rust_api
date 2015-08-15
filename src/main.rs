a3607e2555c5fe50743694417490c7d4f7863334
berleon <github@leon-sixt.de>
2015-08-15 21:40:59 +0200

    add a simple message api

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,26 @@ extern crate router;
 use iron::{Iron, Request, Response, IronResult};
 use iron::status;
 use router::{Router};
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let mut messages = Vec::new();
+    let mut raw_messages : Vec<String> = Vec::new();
+    let mut messages = Arc::new(Mutex::new(raw_messages));
+    let mut messages_clone = messages.clone();
     let mut router = Router::new();
 
-    let all_messages_handler = |req: &mut Request| -> IronResult<Response> {
-            Ok(Response::with((status::Ok, messages.connect("\n"))))
+    fn all_messages_handler(req: &mut Request, msgs: &Vec<String>) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, msgs.connect("\n"))))
     };
 
-    let handler =|req: &mut Request| -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>()
-            .unwrap().find("query").unwrap_or("/");
-        messages.push(*query);
-            Ok(Response::with((status::Ok, "Hallo ".to_string() + *query)))
+    fn handler(req: &mut Request, msgs: &mut Vec<String>) -> IronResult<Response> {
+        let query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        msgs.push(query.to_string());
+        Ok(Response::with((status::Ok, query)))
     };
 
-    router.get("/", handler);
-    router.get("/msg/:query", handler);
-    router.get("/all", all_messages_handler);
+    router.get("/msg/:query",  move |r: &mut Request| handler(r, &mut messages.lock().unwrap()));
+    router.get("/all", move |r: &mut Request| all_messages_handler(r, &messages_clone.lock().unwrap()));
 
     Iron::new(router).http("localhost:3000").unwrap();
-
-
 }
