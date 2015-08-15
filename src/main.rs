452699682ab060605b4a472419ba473bb226e7fc
berleon <github@leon-sixt.de>
2015-08-15 20:14:52 +0200

    use routing example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 extern crate iron;
+extern crate router;
 
-use iron::prelude::*;
+// To build, $ cargo test
+// To use, go to http://127.0.0.1:3000/test
+
+use iron::{Iron, Request, Response, IronResult};
 use iron::status;
+use router::{Router};
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello world!")))
-    }).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+    router.get("/", handler);
+    router.get("/:query", handler);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
 }
